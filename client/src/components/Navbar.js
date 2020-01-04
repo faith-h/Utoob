6e628cefea5e77ecbd60e93b11b956b7e3a713e2
Faith <faithhafen@gmail.com>
2020-01-04 11:11:08 -0700

    navbar halfway done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
-import { Menu, } from 'semantic-ui-react'
+import { Menu, Image, Button } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
 
 class Navbar extends React.Component {
+  // state = { toggle: false }
+
+  // toggleUserView = () => this.setState({ toggle: !this.state.toggle})
 
   rightNavItems = () => {
     const { auth: { user, handleLogout, }, location, } = this.props;
 
     if (user) {
       return (
+        <>
         <Menu.Menu position='right'>
+        <Menu.Item>
+          <Image circular src={require('../images/user.png')} 
+            as='a'
+            size='tiny'
+            href='localhost:3000/profile'
+            // onClick={this.toggleUserView()}
+            />
+          </Menu.Item>
           <Menu.Item
             name='logout'
             onClick={() => handleLogout(this.props.history)}
           />
         </Menu.Menu>
+        <Menu.Item>
+          <Button as={ Link }
+            color='red'
+            size='tiny'
+            to='/videoform'
+            // figure out route to video form
+            > Upload Video 
+            </Button>
+        </Menu.Item>
+      </>
       )
     } else {
       return (
@@ -43,13 +65,20 @@ class Navbar extends React.Component {
     return (
       <div>
         <Menu pointing secondary>
-          <Link to='/'>
+          <Menu.Item>
+          <Image src={require('../images/logo.png')} 
+          as='a'
+          size='small'
+          href='localhost:3000'
+          />
+          </Menu.Item>
+          {/* <Link to='/'>
             <Menu.Item
               name='home'
               id='home'
               active={this.props.location.pathname === '/'}
             />
-          </Link>
+          </Link> */}
           { this.rightNavItems() }
         </Menu>
       </div>
