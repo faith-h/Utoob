9ff29e6c22bace7542d965232406aefdd21fcd1e
Faith <faithhafen@gmail.com>
2020-01-04 11:18:43 -0700

    seeds working, comments may not be created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 9.times do
   comment_num = rand(1..3)
   video = Video.create(
-    title = Faker::Book.title
-    duration = Faker::Number.decimal(l_digits: 2) 
-    genre = Faker::Book.genre 
-    desc = Faker::Food.description 
-    trailer = Faker::Placeholdit.image
+    title: Faker::Book.title,
+    duration: Faker::Number.decimal(l_digits: 2), 
+    genre: Faker::Book.genre, 
+    desc: Faker::Food.description, 
+    trailer: Faker::Placeholdit.image
   )
   comment_num.times do
     Comment.create(
-      body = Faker::GreekPhilosophers.quote
+      body: Faker::GreekPhilosophers.quote
   )
     end
   end
+
+  puts "DB seeded"
