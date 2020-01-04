6dc87214d95e6a7a8fd120236b96cc403f3f2984
Adam Venord <AdamVenord7@gmail.com>
2020-01-04 16:38:07 -0700

    uh oh

--- client/src/components/MyVideos.js
+++ client/src/components/MyVideos.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import { Header, } from "semantic-ui-react"
+import Iframe from 'react-iframe'
 
 const MyVideos = () => (
+  <>
   <Header>MyVideos</Header>
+  <Iframe width="1120" 
+  height="630" 
+  src="https://www.youtube.com/embed/1ihCEM_Cta4" 
+  frameborder="0" 
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+  </Iframe>
+
+  <Iframe width="1120" 
+  height="630" 
+  src="https://www.youtube.com/embed/UtF6Jej8yb4" 
+  frameborder="0" 
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+  </Iframe>
+
+  <Iframe width="1120" 
+  height="630" 
+  src="https://www.youtube.com/embed/-J7J_IWUhls"
+  frameborder="0" 
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+  </Iframe>
+  </>
 )
 
 export default MyVideos
