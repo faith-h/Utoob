dc99bf16b36be9e69908f5610e32680331204ce2
Adam Venord <AdamVenord7@gmail.com>
2020-01-04 14:04:58 -0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Login from './components/Login';
 import Register from './components/Register';
 import FetchUser from './components/FetchUser';
 import VideoForm from './components/VideoForm';
+import Profile from './components/Profile'
+import MyVideos from './components/MyVideos'
+import MyFriends from './components/MyFriends'
 import ProtectedRoute from './components/ProtectedRoute';
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
@@ -20,6 +23,9 @@ const App = () => (
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/videoform" component={VideoForm} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/myvideos" component={MyVideos} />
+          <Route exact path="/myfriends" component={MyFriends} />
           <Route component={NoMatch} />
         </Switch>
       </Container>
